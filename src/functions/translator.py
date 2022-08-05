1fd57c564bed38c8edc679bf285737823ef45754
Rolf Guescini <rolf.guescini@gmail.com>
2022-08-05 12:05:58 +0200

    Capitalization script added

--- src/functions/translator.py
+++ src/functions/translator.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
+import json
+
 from googletrans import Translator
 from rdflib import Graph, Literal, Namespace, RDF, URIRef
 from rdflib.namespace import SKOS, DC, RDFS
+import re
 
 
 class RDFTranslator:
@@ -40,39 +43,51 @@ class RDFTranslator:
         return self.g
 
     def capitalizenames(self, language):
-        qres = self.g.query(
-            """SELECT ?label
-                WHERE {
-                    ?s ?p ?label
-                    FILTER langMatches( lang(?label), "en" )
-                }"""
-        )
-        for row in qres:
-            print(row.label)
-        # for s, p, o in self.g:
-        #     if not (s, p, o) in self.g:
-        #         raise Exception("Iterator / Container Protocols are Broken!!")
-        #     if p.__str__() == 'http://www.w3.org/2004/02/skos/core#prefLabel':
-        #         print("SUBJECT")
-        #         print(s)
-        #         print("PREDICATE")
-        #         print(p)
-        #         print("OBJECT")
-        #         print(o)
+        g2 = Graph()
+        names = self.fetchcapitalizednames(language)
 
+        for subj, pred, obj in self.g.triples((None, SKOS.prefLabel, None)):
+            if (subj, pred, obj) not in self.g:
+                raise Exception("It better be!")
+            print(" ------------------------------------------------------------------------- ")
+            print(" SETTING " + names[subj][language] + " IN LANGUAGE " + language + " FOR ")
+            print(subj)
+            self.g.remove((subj, SKOS.prefLabel, obj))
+            self.g.remove((subj, RDFS.label, obj))
+            self.g.add((subj, SKOS.prefLabel, names[subj][language]))
+            self.g.add((subj, RDFS.label, names[subj][language]))
+
+            self.g.add((subj, RDFS.label, names[subj][obj.language]))
+            self.g.add((subj, SKOS.prefLabel, names[subj][obj.language]))
+            print(" SETTING " + names[subj][obj.language] + " IN LANGUAGE " + obj.language + " FOR ")
+            print(subj)
+            print(" ------------------------------------------------------------------------- ")
+
+        return self.g
+
+    def fetchcapitalizednames(self, language):
+        names = {}
+        for subj, pred, obj in self.g.triples((None, SKOS.prefLabel, None)):
+            if (subj, pred, obj) not in self.g:
+                raise Exception("It better be!")
+            if subj not in names:
+                names[subj] = {}
+            if obj is not None and obj.language == language:
+                capitalizedpreflabel = Literal(self.capitalizestring(obj), lang=language)
+                names[subj][language] = capitalizedpreflabel
+            elif obj is not None and obj.language != language:
+                names[subj][obj.language] = Literal(obj, obj.language)
+        return names
 
     def translatestring(self, inputstring):
-        returnedstring = ""
         translations = self.translator.translate(inputstring, dest=self.destination, src=self.source)
-        returnedstring = self.capitalizestring(translations)
+        returnedstring = self.capitalizestring(translations.text)
         return returnedstring
 
     def capitalizestring(self, inputstring):
-        returnedstring = ""
+        inputstring = self.clean_data(inputstring)
+        inputstring = re.sub(" {2,}", " ", inputstring.strip())
+        return ' '.join([s[0].upper() + s[1:] for s in inputstring.split(' ')])
 
-        if inputstring.text.find(",") > -1:
-            returnedstring = inputstring.text.title()
-        else:
-            returnedstring = inputstring.text.capitalize()
-
-        return returnedstring
+    def clean_data(self, data):
+        return data.replace('\u200c', '')
--- src/main.py
+++ src/main.py
@@ -1,21 +1,39 @@
 # -*- coding: utf-8 -*-
+import json
 
 from functions.translator import RDFTranslator
 
-rdftranslator = RDFTranslator('../faecherklassifikation.rdf', 'de', 'en')
-rdfcapitalizer = RDFTranslator('../faecherklassifikation_en.rdf', 'de', 'en')
 
-# skosgraph = rdftranslator.processrdf()
+# Do initial automatic translation over the Goodle Translate API
+rdftranslator = RDFTranslator('../faecherklassifikation.rdf', 'de', 'en')
+skosgraph = rdftranslator.processrdf()
+#
+print("Serializing graph to XMl/RDF")
+with open('../faecherklassifikation_en_manual.rdf', 'w') as file_object:
+    file_object.write(skosgraph.serialize(format='xml'))
+
+print("Serializing graph to turtle format")
+with open('../faecherklassifikation_en_manual.ttl', 'w') as file_object:
+    file_object.write(skosgraph.serialize())
+
+print("Serializing graph to n3 format")
+with open('../faecherklassifikation_en_manual.ttl', 'w') as file_object:
+    file_object.write(skosgraph.serialize(format="n3"))
+
+
+# If needed, Capitalize Literals after manual Proof reading
+
+# rdfcapitalizer = RDFTranslator('../faecherklassifikation_en_manual.ttl', 'de', 'en')
+# names = rdfcapitalizer.capitalizenames('en')
+#
+# print("Serializing capitalized graph to turtle format")
+# with open('../faecherklassifikation_en.ttl', 'w') as file_object:
+#     file_object.write(names.serialize())
 #
 # print("Serializing graph to XMl/RDF")
 # with open('../faecherklassifikation_en.rdf', 'w') as file_object:
-#     file_object.write(skosgraph.serialize(format='xml'))
-#
-# print("Serializing graph to turtle format")
-# with open('../faecherklassifikation_en.ttl', 'w') as file_object:
-#     file_object.write(skosgraph.serialize())
+#     file_object.write(names.serialize(format='xml'))
 #
 # print("Serializing graph to n3 format")
-# with open('../faecherklassifikation_en.ttl', 'w') as file_object:
-#     file_object.write(skosgraph.serialize(format="n3"))
-rdfcapitalizer.capitalizenames('en')
+# with open('../faecherklassifikation_en.n3', 'w') as file_object:
+#     file_object.write(names.serialize(format="n3"))
